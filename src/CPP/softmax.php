2300a62fb85312e709b0e0f35b4dfac39170dacf
Antonio Gallo <antoniog.web@gmail.com>
2026-02-01 23:53:01 +0100

    add e softmax

---
+++ src/CPP/softmax.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHP2xAI\Tensor\Tensor;
+use PHP2xAI\Runtime\CPP\GraphRuntimeCpp;
+
+include("../../vendor/autoload.php");
+
+$a = Tensor::createFromData([
+							[[1,2],[2,3]],
+							[[1,4],[1,1]]
+							 ],"a");
+
+$a->printData();
+
+$b = $a->softmax(-1);
+
+// print_r($b->context->export());
+
+$graphRuntime = GraphRuntimeCpp::createFromOutputTensor($b);
+$graphRuntime->forward();
+$graphRuntime->backward();
+$graphRuntime->refreshTensorsData(); // reload tensors data and grad after forward and backward
+
+$b->printData();
+$a->printGrad();
